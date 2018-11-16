9f13d2aef215a8c2df0d76f5766dd5737df516be
Unknown <zehurzeda@gmail.com>
2018-11-16 14:01:48 -0200

    Inicio do projeto

---
+++ backend/src/test/java/com/softplan/fullstack/softplan/SoftplanApplicationTests.java
@@ -0,0 +1,16 @@
+package com.softplan.fullstack.softplan;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SoftplanApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
