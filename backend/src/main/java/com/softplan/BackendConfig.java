b2309a5ea341b282e78bf0f371c7bef10b464baa
Unknown <zehurzeda@gmail.com>
2018-11-20 17:39:31 -0200

    Alterações para remover warnings

--- backend/src/main/java/com/softplan/BackendConfig.java
+++ backend/src/main/java/com/softplan/BackendConfig.java
@@ -3,7 +3,6 @@ package com.softplan;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
--- backend/src/main/java/com/softplan/security/TokenProvider.java
+++ backend/src/main/java/com/softplan/security/TokenProvider.java
@@ -22,7 +22,9 @@ import java.util.stream.Collectors;
 @Component
 public class TokenProvider implements Serializable {
 
-    public String getUsernameFromToken(String token) {
+	private static final long serialVersionUID = 1L;
+
+	public String getUsernameFromToken(String token) {
         return getClaimFromToken(token, Claims::getSubject);
     }
 
