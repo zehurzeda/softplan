80be99f47030d82bbf044bedbfc82a660213810f
Unknown <zehurzeda@gmail.com>
2018-11-20 16:22:55 -0200

    Correção de bug em pesquisa

--- backend/src/main/java/com/softplan/model/UsuarioParecerProcesso.java
+++ backend/src/main/java/com/softplan/model/UsuarioParecerProcesso.java
@@ -1,16 +1,11 @@
 package com.softplan.model;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
 import javax.validation.constraints.NotNull;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class UsuarioParecerProcesso {
--- backend/src/main/java/com/softplan/repository/UsuarioRepository.java
+++ backend/src/main/java/com/softplan/repository/UsuarioRepository.java
@@ -12,6 +12,13 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
 	List<Usuario> findAllByRoles_Nome(String roleNome);
 
-	@Query("select DISTINCT(u) from Usuario u left join u.roles ur left join u.pareceresUsuario pu left join pu.processo p where ur.nome = ?1 and (pu is null or pu.pendente = false) or p.id <> ?2")
+	@Query("select DISTINCT(u) from Usuario u "
+			+ "left join u.roles ur "
+			+ "left join u.pareceresUsuario pu "
+			+ "left join pu.processo p "
+			+ "where ur.nome = ?1 "
+			+ "and (pu is null or p.id <> ?2) "
+			+ "or (select upp.id from UsuarioParecerProcesso upp where upp.usuario.id = u.id and upp.processo = p and upp.pendente = true) is null")
 	List<Usuario> findAllUsuariosDisponiveisByRole(String roleNome, long idProcesso);
 }
+	
